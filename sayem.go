f92e1262168d3d2f3cc7d1a4be1c0899f0926f61
sam43 <scode43@gmail.com>
2016-04-28 23:14:05 +0600

    Commit-for-go program
    **Description
    #We want to calculate a sum of squares of some integers, excepting negatives
    #The first line of the input will be an integer N (1 <= N <= 100)
    #Each of the following N test cases consists of one line containing an integer X (0 < X <= 100), followed by X integers (Yn, -100 <= Yn <= 100) space-separated on the next line
    #For each test case, calculate the sum of squares of the integers excepting negatives, and print the calculated sum to the output. No blank line between test cases
    #(Take input from standard input, and output to standard output)

---
+++ sayem.go
@@ -0,0 +1,70 @@
+package main
+
+
+import "fmt"
+
+
+func sumElement(n int, total *int) {
+    
+if n == 0 {
+        
+return
+    
+}
+    
+    
+var element int
+    
+fmt.Scan(&element)
+    
+    
+if element >= 0 {
+
+        (*total) += (element * element)
+    
+}
+    
+    
+n--
+    
+sumElement(n, total)
+}
+
+
+func handleTestcase(n int) {
+ 
+   if n == 0 {
+
+        return
+    }
+ 
+   
+    var elements int
+
+    fmt.Scan(&elements)
+  
+  
+    var total = 0
+
+    
+sumElement(elements, &total)
+
+    fmt.Println(total)
+
+   
+ n--
+    
+handleTestcase(n)
+
+}
+
+
+func main() {
+    
+var testcase int
+    
+fmt.Scan(&testcase)    
+    
+handleTestcase(testcase)
+
+}
